346e2e2003ea376bb422df91e383875afff6064b
Alt B 2022 <massie.w@gmail.com>
2023-03-10 15:11:50 -0800

    Created associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,19 +5,23 @@ const Comment = require("./comment")
 //Associations - what belongs to what
 
 //blog posts belong to users
-//fk = user_id
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+})
 
 //comments belong to user
-//fk = user_id
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
 
 //post has many comments
 //fk = post_id
+Blog.hasMany(Comment, {
+    foreignKey: 'post_id',
+})
 
 //hasOne is another type
 
 //belongsToMany, belongsToOne - read docs
-
-
-
 
 module.exports = { User, Blog, Comment };
