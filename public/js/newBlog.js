0ec96abab359c75cf831e6b53a87a929dcf22a96
Alt B 2022 <massie.w@gmail.com>
2023-03-16 10:11:54 -0700

    Added code for homepage.handlebars

---
+++ public/js/newBlog.js
@@ -0,0 +1,12 @@
+const newFormHandler = async (event) => {
+	event.preventDefault();
+	event.preventDefault();
+
+
+
+
+
+
+};
+
+document.querySelector(".new-blog-post-form").addEventListener("submit", newFormHandler);
